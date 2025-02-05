a4cce5c693454559ef57aa9f87482537e2dde153
Andersonmia2222 <mia.anderson2026@gbwl.org>
2025-02-05 17:55:17 +0000

    python

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-def main():
-    print("hello world")
+#   Variables store data 
+# "Number " is the name of the variable 
+# 11 is the value
+number = 11 
+
+#these are STRINGS(word/sentnce/letter)
+#STRINGS need quotes around them
+name="Mia"
+#print is a FUNCTION(process/action)
+print(name) 
+
+#change the value of a variable 
+name= "Katerina"
+print(name)
 
 
-if __name__ == "__main__":
-    main()
+#How to combine STRINGS
+greeting="hello"
+print(greeting + " "+ name) 
+
+#these are all integers(whole numbers) 
+x=4
+y=-10
+z=0
+
+#these are floats (decimals)
+x=5.0
+y=3.4
+z=-1.0
+
+#math operations with numbers
+sum= x+y+z
+print(sum )
